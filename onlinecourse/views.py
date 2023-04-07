c6944dc1259740adadf4b0f1af8f53dc0fad271e
fontduroy <fontduroy@gmail.com>
2023-04-07 13:20:17 -0400

    get questions in there

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -90,6 +90,7 @@ class CourseDetailView(generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['questions'] = Question.objects.filter(course=context['object'])
         return context
 
 
